77cf093d9c8db58056f5f7f37fce7c9e5b00815e
haow <weix.hao@intel.com>
2019-01-23 17:45:27 +0800

    pipenv lock

--- flashmrb.py
+++ flashmrb.py
@@ -7,6 +7,7 @@ import fire
 import zipfile
 import requests
 import serial
+import sh 
 from tqdm import tqdm 
 requests.packages.urllib3.disable_warnings()
 
@@ -15,17 +16,19 @@ class flash_mrb(object):
         if call(r"ls /dev/ttyUSB*", shell=True):
             raise Exception("not inset debug borad")
         call(r"pkill minicom", shell=True)
-        self.ser = serial.Serial('/dev/ttyUSB2')
+        time.sleep(1)
+        self.ser2 = serial.Serial('/dev/ttyUSB2')
+        self.ser3 = serial.Serial('/dev/ttyUSB3')
         self.filepath = os.getcwd() + os.sep + time.strftime('ww%W%w') + os.sep
-        shutil.rmtree(self.filepath, ignore_errors=True)
-        os.mkdir(self.filepath)
 
     def downfile(self, url):
         # wget = Popen(r"wget {url} --no-check-certificate  -P {filepath} --http-user=haowx --http-password='hw!QAZ1qaz' --no-proxy".format(filepath=self.filepath, url=url), shell=True)
+        shutil.rmtree(self.filepath, ignore_errors=True)
+        os.mkdir(self.filepath)
         r = requests.get(url, auth=('haowx', 'hw!QAZ1qaz'), verify=False, stream=True)
         if r.status_code != 200:
             raise Exception("download file fail")
-        length = float(r.headers['content-length'])/512
+        length = int(r.headers['content-length'])/512
         pbar = tqdm(total=length)
         with open(self.filepath + 'test.zip', 'wb') as f:
             for chunk in r.iter_content(chunk_size=512):
@@ -36,7 +39,14 @@ class flash_mrb(object):
         with zipfile.ZipFile(self.filepath + 'test.zip') as f:
             f.extractall(self.filepath)
 
-    # def
+    def cflasher(self):
+        if not self.ser2.isOpen():
+            raise Exception("is not open")
+        self.ser2.write(b'r')
+        if self.ser2.read().find("Logic Core Initialisation done"):
+            self.ser2.write(b'n1#')
+
+
 
 if __name__ == '__main__':
     fire.Fire(flash_mrb)
